ad3514bfff1627cc44e709b004a2a15c44169177
desolat <nuabaranda@web.de>
2013-03-22 17:42:11 +0100

    refactored tests for pytest, experiments with BVLC length calculation

--- src/bacnet.py
+++ src/bacnet.py
@@ -15,48 +15,41 @@ __version__ = ""
 # $Source$
 
 
-import sys
-import logging
+import inspect
 
 from scapy.all import *
 from scapy.layers.inet import IP, UDP
-from scapy.config import conf as scapy_conf
-
-from itf.commons.commons import Enum
-from netaddr.ip import IPAddress, IPNetwork
-from itf.commons.exception import ArgumentError
-from itf.btf.helper import BtfHelper
-
-#logging.getLogger("scapy").setLevel(logging.DEBUG)
-#log_runtime.setLevel(logging.DEBUG)
-#log_interactive.setLevel(logging.DEBUG)
-#log_loading.setLevel(logging.DEBUG)
-
-log = logging.getLogger()
 
 
-class StreamToLogger(object):
-    """
-    Fake file-like stream object that redirects writes to a logger instance.
-    """
+BACNET_PORT = 47808
 
-    def __init__(self, logger, log_level=logging.INFO):
-        self.logger = logger
-        self.log_level = log_level
-        self.linebuf = ''
- 
-    def write(self, buf):
-        for line in buf.rstrip().splitlines():
-            self.logger.log(self.log_level, line.rstrip())
- 
-stdout_logger = logging.getLogger('STDOUT')
-sl = StreamToLogger(stdout_logger, logging.INFO)
-sys.stdout = sl
- 
-stderr_logger = logging.getLogger('STDERR')
-sl = StreamToLogger(stderr_logger, logging.ERROR)
-sys.stderr = sl
 
+class Enum(object):
+    _intern = object.__dict__.keys() + ['__module__', '__dict__', '__weakref__', '_intern']
+    
+    @classmethod
+    def keys(cls):
+        # introspection on __dict__ values does not work
+#        keys = filter(lambda key: key not in cls._intern, 
+#                      cls.__dict__.keys())
+        members = inspect.getmembers(cls)
+        keys = [key for key, value in members if (key not in cls._intern 
+                                                  and not inspect.ismethod(value)
+                                                  and not inspect.isfunction(value))]
+        return keys
+
+    @classmethod
+    def values(cls):
+        return [cls.__dict__[key] for key in cls.keys()]
+    
+    @classmethod
+    def dict(cls):
+        return dict(zip(cls.keys(), cls.values()))
+    
+    @classmethod
+    def revDict(cls):
+        return dict(zip(cls.values(), cls.keys()))
+    
 
 class BvlcFunction(Enum):
     RESULT = 0
@@ -69,25 +62,82 @@ class BvlcFunction(Enum):
     ORIGINAL_BROADCAST_NPDU = 11
 
 
+class BVLCConditional(Packet):
+    name = 'BVLC-CONDITIONAL'
+    fields_desc = [
+                   ConditionalField(ShortField('time_to_live', None),
+                                    lambda pkt: pkt.underlayer.function == BvlcFunction.REGISTER_FD),
+                   ConditionalField(IPField('origin_ip', None), 
+                                    lambda pkt: pkt.underlayer.function == BvlcFunction.FORWARDED_NPDU),
+                   ConditionalField(ShortField('origin_port', 47808), 
+                                    lambda pkt: pkt.underlayer.function == BvlcFunction.FORWARDED_NPDU)
+                   ]
+    
+    def extract_padding(self, s):
+        return "", s
+
+    
+#def calcBVLCLength(pkt, fieldLen):
+#    length = 4
+#    length += (len(pkt.time_to_live) if pkt.time_to_live is not None else 0)
+#    length += (len(pkt.origin_ip) if pkt.origin_ip is not None else 0)
+#    length += (len(pkt.origin_port) if pkt.origin_port is not None else 0)
+#    length += len(pkt.payload)
+#    return length
+
+    
+class BVLC(Packet):
+    name = 'BVLC'
+    fields_desc = [
+                   XByteField('type', 0x81),
+                   ByteEnumField('function', None, BvlcFunction.revDict()),
+
+#                   LenField('length', None),
+#                   FieldLenField('length', None, length_of='type',
+#                                 adjust=calcBVLCLength), 
+#                                 adjust=lambda pkt,x: 
+#                                    x + \
+#                                    3 + \
+#                                    (len(pkt.time_to_live) if pkt.time_to_live is not None else 0) + \
+#                                    (len(pkt.origin_ip) if pkt.origin_ip is not None else 0) + \
+#                                    (len(pkt.origin_port) if pkt.origin_port is not None else 0) + \
+#                                    len(pkt.payload)),
+#                   ConditionalField(ShortField('time_to_live', None),
+#                                    lambda pkt: pkt.function == BvlcFunction.REGISTER_FD),
+#                   ConditionalField(IPField('origin_ip', None), 
+#                                    lambda pkt: pkt.function == BvlcFunction.FORWARDED_NPDU),
+#                   ConditionalField(ShortField('origin_port', 47808), 
+#                                    lambda pkt: pkt.function == BvlcFunction.FORWARDED_NPDU),
+
+                   FieldLenField('length', None, length_of='conditional', 
+                                 adjust=lambda pkt,x: len(pkt.payload)+x+4),
+                   PacketListField("conditional", [], BVLCConditional, 
+                                   length_from=lambda pkt: pkt.length - len(pkt.payload) - 4)   
+                   ]
+    
+#    def post_build(self, pkt, pay):
+#        if self.length is None:
+#            length = 4 + len(pay)
+#            if self.time_to_live is not None:
+#                length += 2
+#            if self.origin_ip is not None: 
+#                length += 4
+#            if self.origin_port is not None:
+#                length += 4
+#            self.length = length
+#        
+#        return Packet.post_build(self, pkt, pay)
+
+    def extract_padding(self, s):
+        return "", s
+
+
 class BVLCBase(Packet):
     name = 'BVLC-BASE'
     fields_desc = [XByteField('type', 0x81),
                    ByteEnumField('function', BvlcFunction.RESULT, BvlcFunction.revDict()),
                    ]
-
-class BVLC(Packet):
-    name = 'BVLC'
-    fields_desc = [
-                   PacketListField('bvlc_base',
-                                   None, 
-                                   BVLCBase),
-                   ConditionalField(ShortField('time_to_live', None),
-                                    lambda pkt: pkt.function == BvlcFunction.READ_BDT),
-                   ConditionalField(IPField('origin_ip', None), 
-                                    lambda pkt: pkt.function == BvlcFunction.FORWARDED_NPDU),
-                   ConditionalField(ShortField('origin_port', 47808), 
-                                    lambda pkt: pkt.function == BvlcFunction.FORWARDED_NPDU),
-                   ]
+    
 
 class BVLCReadBDT(Packet):
     name = 'BVLC-READ-BDT'
@@ -149,6 +199,29 @@ class NPDUSource(Packet):
                    ]
 
 
+class NetworkLayerMessageType(Enum):
+    WHO_IS_ROUTER_TO_NETWORK = 0x00
+    I_AM_ROUTER_TO_NETWORK = 0x01
+
+
+class NPDU(Packet):
+    name = 'NPDU'
+    fields_desc = [
+                   ByteField('version', 1),
+                   BitField('nlpci', 0b00000000, 8),
+                   # @todo: use bit matching as condition
+                   ConditionalField(PacketListField('dest', None, NPDUDest), 
+                                    lambda pkt: pkt.hop_count is not None), 
+                   ConditionalField(PacketListField('source', None, NPDUSource),
+                                    lambda pkt: pkt.hop_count is not None),
+                   ConditionalField(ByteField('hop_count', None), 
+                                    lambda pkt: pkt.hop_count is not None),
+                   XByteField('message_type', None),
+                   ConditionalField(ShortField('network', None), 
+                                    lambda pkt: pkt.message_type == NetworkLayerMessageType.WHO_IS_ROUTER_TO_NETWORK)
+                   ]
+
+
 class NPDUBase(Packet):
     name = 'NPDU_BASE'
     fields_desc = [
@@ -162,12 +235,7 @@ class NPDUBase(Packet):
                    ConditionalField(ByteField('hop_count', None), 
                                     lambda pkt: pkt.hop_count is not None)
                    ]
-
-
-class NetworkLayerMessageType(Enum):
-    WHO_IS_ROUTER_TO_NETWORK = 0x00
-    I_AM_ROUTER_TO_NETWORK = 0x01
-
+    
     
 class NPDUContentBase(Packet):
     name = 'NPDU-CONTENT-BASE'
@@ -198,8 +266,8 @@ class NPDUIAmRouterToNetworkContent(NPDUContentBase):
                    ]
 
     
-class NPDU(Packet):
-    name = 'NPDU'
+class NPDUWithBVLCLength(Packet):
+    name = 'NPDU-WITH-BVLC-LENGTH'
     fields_desc = [FieldLenField('length', None, length_of='content', adjust = lambda pkt,x:x+4),
                    PacketListField('content',
                                    None,
@@ -239,109 +307,4 @@ def getNPDUBase(dest=None, source=None, hopCount=255):
     return npdu    
 
 
-class BacDevStub(object):
-    '''
-    Very simple stub for a BACnet device.
-    '''
-    
-    def __init__(self, srcIP=None, port=None, verb=0):
-        if srcIP is None or port is None:
-            tdIP, tdPort = BtfHelper.getTdIpAndPort()
-        self.srcIpAddr = tdIP if srcIP is None else srcIP
-        self.port = tdPort if port is None else port
-        
-        scapy_conf.verb=verb
-#        scapy_conf.logLevel = 100
-        
-        bind_layers(UDP, BVLCBase, sport=port)
-        bind_layers(UDP, BVLCBase, dport=port)
-        
-        self.ip = IP()
-        self.ip.src = self.srcIpAddr
-        self.udp = self.ip/UDP(sport=port, dport=port)
 
-        
-    def sendBvlcReadBdt(self, destIp):
-        ip = IP()
-        ip.src = self.srcIpAddr
-        ip.dst = destIp
-        send(ip/UDP(sport=self.port, dport=self.port)/BVLCReadBDT)
-
-    def sendBvlcRegFd(self, destIp, ttl):
-        ip = IP()
-        ip.src = self.srcIpAddr
-        ip.dst = destIp
-        send(ip/UDP(sport=self.port, dport=self.port)/BVLCRegisterFD(time_to_live=ttl))
-        
-    
-    def sendNpdu(self, destIp):
-        ip = IP()
-        ip.src = self.srcIpAddr
-        ip.dst = destIp
-        send(ip/UDP(sport=self.port, dport=self.port)/BVLCOrigUnicastNPDU()/NPDUBase())
-
-    def getUDP(self, destIp):
-        ip = IP()
-        ip.src = self.srcIpAddr
-        ip.dst = destIp
-        return ip/UDP(sport=self.port, dport=self.port)
-    
-    def getBVLC(self, ipDest):
-        if isinstance(ipDest, IPNetwork):
-            udp = self.getUDP(str(ipDest.broadcast))
-            bvlc = udp/BVLCOrigBroadcastNPDU()
-        elif isinstance(ipDest, IPAddress):
-            udp = self.getUDP(str(ipDest))
-            bvlc = udp/BVLCOrigUnicastNPDU()
-        else:
-            raise ArgumentError('Invalid class for IP destination')
-        return bvlc
-    
-    def sendIAmRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, nets=None):
-        '''
-        @type ipDest: IPNetwork for broadcast or IPAddress for unicast
-        @param dest: NPDU destination
-        @type dest: Dict with dnet (int) and dadr (string with hex MAC)
-        @param source: NPDU source
-        @type source: Dict with snet and sadr (string with hex MAC)
-        '''
-        
-        content = NPDUIAmRouterToNetworkContent(npdu=getNPDUBase(dest, source, hopCount), 
-                                                networks=nets)
-        iAmRouterToNetwork = NPDU(content=content)
-        p = self.getBVLC(ipDest)/iAmRouterToNetwork
-        p.show2()
-        send(p)
-        
-    def getWhoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
-        content = NPDUWhoIsRouterToNetworkContent(npdu=getNPDUBase(dest, source, hopCount), 
-                                                  network=net)
-        whoIsRouterToNetwork = NPDU(content=content)
-        p = self.getBVLC(ipDest)/whoIsRouterToNetwork
-        return p
-        
-    def sendWhoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
-        p = self.getWhoIsRouterToNetwork(ipDest, dest, source, hopCount, net)
-        send(p)
-        
-    def whoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
-        p = self.getWhoIsRouterToNetwork(ipDest, dest, source, hopCount, net)
-        ans, unans = sr(p, multi=True, timeout=3, verbose=3)
-        for answer in ans:
-            answer.summarize()
-            
-
-def readPCAP(path, port=47808):
-    bind_layers(UDP, BVLCBase, sport=port)
-    bind_layers(UDP, BVLCBase, dport=port)
-    bind_layers(BVLCBase, NPDU,
-                function=BvlcFunction.ORIGINAL_BROADCAST_NPDU)
-    bind_layers(NPDU, NPDUContentBase)
-    bind_layers(NPDU, NPDUBase)
-    
-    packets = rdpcap(path)
-    packets.summary()
-    for packet in packets:
-        packet.summary()
-        npdu = packet['NPDU']
-        npdu.summary()
--- tests/test_bacnet.py
+++ tests/test_bacnet.py
@@ -8,68 +8,238 @@ __version__ = ""
 # $Source$
 
 
-import unittest
+import sys
 import os.path
 
+from scapy.all import *
+from scapy.layers.inet import IP, UDP
 from netaddr.ip import IPNetwork, IPAddress
+import pytest
 
-from itf.btf.dev_stub import BacDevStub, readPCAP
-from tests.init_tests import TEST_DATA
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src'))
+
+from bacnet import BvlcFunction, BVLC, BVLCConditional, BVLCBase, BVLCReadBDT, BACNET_PORT, NPDU,\
+    NetworkLayerMessageType
+
 
 SRC_IP = '192.168.88.1'
 DST_IP = '168.152.32.39'
 DST_BROADCAST = '168.152.32.255'
 DST_CIDR = '24'
-PORT = 47808
 
 PCAP_PATH = os.path.normpath("")
 
 
-class TestBacDevStub(unittest.TestCase):
+@pytest.fixture
+def bind_bvlc():
+    bind_layers(UDP, BVLC, sport=BACNET_PORT)
+    bind_layers(UDP, BVLC, dport=BACNET_PORT)
     
-    def setUp(self):
-        self.fakeBacDev = BacDevStub(SRC_IP, PORT, verb=3)
 
-    def _testRegFd(self):
-        self.fakeBacDev.sendBvlcRegFd(DST_IP, ttl=300)
-
-    def _testSendNpdu(self):
-        self.fakeBacDev.sendNpdu(DST_IP)
-        
-    def _testSendIAmRouterToNetworkIPBroadcast(self):
-        self.fakeBacDev.sendIAmRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
-                                                         dest={'dnet' : 99, 'dadr' : '01'}, 
-                                                         nets=[8,9])
-
-    def _testSendWhoIsRouterToNetworkIPUnicast(self):
-        self.fakeBacDev.sendWhoIsRouterToNetwork(IPAddress(DST_IP), 
-                                                 dest={'dnet' : 99, 'dadr' : '01'}, net=78)
-
-    def _testSendWhoIsRouterToNetworkIPBroadcast(self):
-        self.fakeBacDev.sendWhoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
-                                                           dest={'dnet' : 99, 'dadr' : '01'}, 
-                                                           net=78)
-
-    def _testSendWhoIsRouterToNetworkDestBroadcast(self):
-        self.fakeBacDev.sendWhoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
-                                                 dest={'dnet' : 99, 'dlen' : 0}, net=78)    
-
-    def _testWhoIsRouterToNetwork(self):
-        self.fakeBacDev.whoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])))
-        
-    def testReadPCAP(self):
-        readPCAP(os.path.join(TEST_DATA, 'pcap', 'who_is_router_to_network.pcap'))
-
-def suite():
-    tests = []
-    testLoader = unittest.TestLoader()
-    for testCase in [
-                     TestBacDevStub
-                              ]:
-        tests.extend(testLoader.loadTestsFromTestCase(testCase))
-    return unittest.TestSuite(tests)
+@pytest.fixture
+def udp():
+    udp = IP(src=SRC_IP, dst=DST_IP)/UDP(sport=BACNET_PORT, dport=BACNET_PORT)
+    return udp
 
 
-if __name__ == "__main__": 
-    unittest.main()
-#    unittest.TextTestRunner(verbosity=2).run(suite())
+@pytest.mark.usefixtures('bind_bvlc')
+def test_bvlc_read_bdt(udp):
+    bvlc = udp/BVLC(function=BvlcFunction.READ_BDT)
+    bvlc.show2()
+    send(bvlc)
+    assert True == False
+
+
+@pytest.mark.usefixtures('bind_bvlc')
+def test_bvlc_register_fd(udp):
+    bind_layers(BVLC, BVLCConditional)
+    bvlc = udp/BVLC(function=BvlcFunction.REGISTER_FD)/BVLCConditional(time_to_live=60)
+    bvlc.show2()
+    send(bvlc)
+    assert True == False
+
+
+def test_send_bvlc_read_bdt(udp):
+    bind_layers(UDP, BVLCBase, sport=BACNET_PORT)
+    bind_layers(UDP, BVLCBase, dport=BACNET_PORT)
+    
+    bind_layers(BVLCBase, BVLCReadBDT, function=BvlcFunction.READ_BDT)
+    
+    p = udp/BVLCReadBDT()
+    p.show()
+    p.show2()
+    send(p)
+    assert True == False
+
+
+@pytest.mark.usefixtures('bind_bvlc')
+def test_npdu_who_is_router_to_network(udp):
+#    bind_layers(BVLC, BVLCConditional)
+    bind_layers(BVLC, NPDU)
+    
+    bvlc = udp/BVLC(function=BvlcFunction.ORIGINAL_BROADCAST_NPDU)
+    npdu = bvlc/NPDU(nlpci=0b10000000, message_type=NetworkLayerMessageType.WHO_IS_ROUTER_TO_NETWORK,
+                     network=1)
+#    npdu.show()
+    npdu.show2()
+    send(npdu)
+    print npdu['BVLC'].length
+    assert npdu['BVLC'].length == 9
+    assert True == False
+
+
+#class BacDevStub(object):
+#    '''
+#    Very simple stub for a BACnet device.
+#    '''
+#    
+#    def __init__(self, srcIP=None, port=None, verb=0):
+#        if srcIP is None or port is None:
+#            tdIP, tdPort = BtfHelper.getTdIpAndPort()
+#        self.srcIpAddr = tdIP if srcIP is None else srcIP
+#        self.port = tdPort if port is None else port
+#        
+#        scapy_conf.verb=verb
+##        scapy_conf.logLevel = 100
+#        
+#        bind_layers(UDP, BVLCBase, sport=port)
+#        bind_layers(UDP, BVLCBase, dport=port)
+#        
+#        self.ip = IP()
+#        self.ip.src = self.srcIpAddr
+#        self.udp = self.ip/UDP(sport=port, dport=port)
+#
+#        
+#    def sendBvlcReadBdt(self, destIp):
+#        ip = IP()
+#        ip.src = self.srcIpAddr
+#        ip.dst = destIp
+#        send(ip/UDP(sport=self.port, dport=self.port)/BVLCReadBDT)
+#
+#    def sendBvlcRegFd(self, destIp, ttl):
+#        ip = IP()
+#        ip.src = self.srcIpAddr
+#        ip.dst = destIp
+#        send(ip/UDP(sport=self.port, dport=self.port)/BVLCRegisterFD(time_to_live=ttl))
+#        
+#    
+#    def sendNpdu(self, destIp):
+#        ip = IP()
+#        ip.src = self.srcIpAddr
+#        ip.dst = destIp
+#        send(ip/UDP(sport=self.port, dport=self.port)/BVLCOrigUnicastNPDU()/NPDUBase())
+#
+#    def getUDP(self, destIp):
+#        ip = IP()
+#        ip.src = self.srcIpAddr
+#        ip.dst = destIp
+#        return ip/UDP(sport=self.port, dport=self.port)
+#    
+#    def getBVLC(self, ipDest):
+#        if isinstance(ipDest, IPNetwork):
+#            udp = self.getUDP(str(ipDest.broadcast))
+#            bvlc = udp/BVLCOrigBroadcastNPDU()
+#        elif isinstance(ipDest, IPAddress):
+#            udp = self.getUDP(str(ipDest))
+#            bvlc = udp/BVLCOrigUnicastNPDU()
+#        else:
+#            raise ArgumentError('Invalid class for IP destination')
+#        return bvlc
+#    
+#    def sendIAmRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, nets=None):
+#        '''
+#        @type ipDest: IPNetwork for broadcast or IPAddress for unicast
+#        @param dest: NPDU destination
+#        @type dest: Dict with dnet (int) and dadr (string with hex MAC)
+#        @param source: NPDU source
+#        @type source: Dict with snet and sadr (string with hex MAC)
+#        '''
+#        
+#        content = NPDUIAmRouterToNetworkContent(npdu=getNPDUBase(dest, source, hopCount), 
+#                                                networks=nets)
+#        iAmRouterToNetwork = NPDU(content=content)
+#        p = self.getBVLC(ipDest)/iAmRouterToNetwork
+#        p.show2()
+#        send(p)
+#        
+#    def getWhoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
+#        content = NPDUWhoIsRouterToNetworkContent(npdu=getNPDUBase(dest, source, hopCount), 
+#                                                  network=net)
+#        whoIsRouterToNetwork = NPDU(content=content)
+#        p = self.getBVLC(ipDest)/whoIsRouterToNetwork
+#        return p
+#        
+#    def sendWhoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
+#        p = self.getWhoIsRouterToNetwork(ipDest, dest, source, hopCount, net)
+#        send(p)
+#        
+#    def whoIsRouterToNetwork(self, ipDest, dest=None, source=None, hopCount=255, net=None):
+#        p = self.getWhoIsRouterToNetwork(ipDest, dest, source, hopCount, net)
+#        ans, unans = sr(p, multi=True, timeout=3, verbose=3)
+#        for answer in ans:
+#            answer.summarize()
+#            
+#
+#def readPCAP(path, port=47808):
+#    bind_layers(UDP, BVLCBase, sport=port)
+#    bind_layers(UDP, BVLCBase, dport=port)
+#    bind_layers(BVLCBase, NPDU,
+#                function=BvlcFunction.ORIGINAL_BROADCAST_NPDU)
+#    bind_layers(NPDU, NPDUContentBase)
+#    bind_layers(NPDU, NPDUBase)
+#    
+#    packets = rdpcap(path)
+#    packets.summary()
+#    for packet in packets:
+#        packet.summary()
+#        npdu = packet['NPDU']
+#        npdu.summary()
+
+#class TestBacDevStub(unittest.TestCase):
+#    
+#    def setUp(self):
+#        self.fakeBacDev = BacDevStub(SRC_IP, PORT, verb=3)
+#
+#    def _testRegFd(self):
+#        self.fakeBacDev.sendBvlcRegFd(DST_IP, ttl=300)
+#
+#    def _testSendNpdu(self):
+#        self.fakeBacDev.sendNpdu(DST_IP)
+#        
+#    def _testSendIAmRouterToNetworkIPBroadcast(self):
+#        self.fakeBacDev.sendIAmRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
+#                                                         dest={'dnet' : 99, 'dadr' : '01'}, 
+#                                                         nets=[8,9])
+#
+#    def _testSendWhoIsRouterToNetworkIPUnicast(self):
+#        self.fakeBacDev.sendWhoIsRouterToNetwork(IPAddress(DST_IP), 
+#                                                 dest={'dnet' : 99, 'dadr' : '01'}, net=78)
+#
+#    def _testSendWhoIsRouterToNetworkIPBroadcast(self):
+#        self.fakeBacDev.sendWhoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
+#                                                           dest={'dnet' : 99, 'dadr' : '01'}, 
+#                                                           net=78)
+#
+#    def _testSendWhoIsRouterToNetworkDestBroadcast(self):
+#        self.fakeBacDev.sendWhoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])), 
+#                                                 dest={'dnet' : 99, 'dlen' : 0}, net=78)    
+#
+#    def _testWhoIsRouterToNetwork(self):
+#        self.fakeBacDev.whoIsRouterToNetwork(IPNetwork('/'.join([DST_BROADCAST, DST_CIDR])))
+#        
+#    def testReadPCAP(self):
+#        readPCAP(os.path.join(TEST_DATA, 'pcap', 'who_is_router_to_network.pcap'))
+
+#def suite():
+#    tests = []
+#    testLoader = unittest.TestLoader()
+#    for testCase in [
+#                     TestBacDevStub
+#                              ]:
+#        tests.extend(testLoader.loadTestsFromTestCase(testCase))
+#    return unittest.TestSuite(tests)
+#
+#
+#if __name__ == "__main__": 
+#    unittest.main()
+##    unittest.TextTestRunner(verbosity=2).run(suite())
